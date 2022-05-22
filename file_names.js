b5ca0a2bd8a1995025c5bdfd051de85f09ee21d8
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-05-22 22:26:36 +0300

    init: create the basis for the task: File names

---
+++ file_names.js
@@ -0,0 +1,10 @@
+function renameFiles(array) {
+  let result = [];
+  return result;
+}
+
+// Test:
+console.log(renameFiles(["file", "file", "image", "file(1)", "file"]));
+
+// Result:
+// ["file", "file(1)", "image", "file(1)(1)", "file(2)"]

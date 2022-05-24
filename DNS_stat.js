8ac856a138a2702181917205251c0d6f3957e68f
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-05-24 21:37:35 +0300

    init: start working on a task (DNS stat)

---
+++ DNS_stat.js
@@ -0,0 +1,27 @@
+//_Task
+/*  
+   Your task is to implement function that accepts an array of domains (domains) 
+   and returns the object with the appearances of the DNS.
+*/
+
+//_Function:
+function getDNSStats(domains) {
+  return {};
+}
+
+//_Tests:
+const domains1 = ["code.yandex.ru", "music.yandex.ru", "yandex.ru"];
+
+const domains2 = ["example.com", "example.net", "example.org", "example.edu"];
+
+const domains3 = [
+  "www.google.ru",
+  "mail.google.com",
+  "www.youtube.com",
+  "play.google.com",
+  "calendar.google.com",
+];
+
+console.log(getDNSStats(domains1));
+//console.log(getDNSStats(domains2));
+//console.log(getDNSStats(domains3));

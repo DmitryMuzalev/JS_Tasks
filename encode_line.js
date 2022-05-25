55da61fb675e33aa844ff33d06dc0225f0bd80b4
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-05-25 10:51:46 +0300

    init: start working on a task (Encode line)

---
+++ encode_line.js
@@ -0,0 +1,14 @@
+//_Task:
+/* 
+   Your task is to implement function that accepts string (str) and returns its encoded version.
+   For example: encodeLine('aabbbc') => '2a3bc'
+*/
+
+//_Function:
+function encodeLine(string) {}
+
+//_Tests:
+
+console.log(encodeLine("aabbbc"));
+//console.log(encodeLine("asidjiadj"));
+//console.log(encodeLine("fffasadskaaaa adaisdhaaseee adjijaaaeerrwe"));

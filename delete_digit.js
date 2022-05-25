02edffdbeef2f4162e90585283304fc26642d187
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-05-25 13:01:25 +0300

    init: start working on a task (Delete digit}

---
+++ delete_digit.js
@@ -0,0 +1,18 @@
+//_Task
+/*  
+   Your task is to implement function that accepts integer number (n) and returns maximal
+   number you can obtain by deleting exactly one digit of the given number.
+   
+   For example:
+   deleteDigit(152) => 52
+*/
+
+//_Function:
+function deleteDigit(n) {}
+
+//_Tests:
+console.log(152);
+//console.log(620454011);
+//console.log(7745);
+//console.log(1001);
+//console.log(16);

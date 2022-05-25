1a2bcba55942836e0c701ca7b4fd380bbddd625b
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-05-25 11:18:17 +0300

    init: start working on a task (Common character count}

---
+++ common_character_count.js
@@ -0,0 +1,17 @@
+//_Task:
+/* 
+   Your task is to implement function that accepts two strings (s1 and s2) and returns number of common characters between them.
+   For example:
+   getCommonCharacterCount('aabcc', 'adcaa') => 3
+*/
+
+//_Function:
+function getCommonCharacterCount(s1, s2) {}
+
+//_Tests:
+
+console.log(getCommonCharacterCount("aabcc", "adcaa"));
+//console.log(getCommonCharacterCount("aabdufaudfhauicc", "adcsdfsihdaa"));
+//console.log(getCommonCharacterCount("testString", "testString"));
+//console.log(getCommonCharacterCount("sbdfhbsdf", "gagd"));
+//console.log(getCommonCharacterCount("sbdfh", "gamasdsguigdigfgd"));

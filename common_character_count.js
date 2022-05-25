6138f0a2285951dfcc536baf4710c9df13f48f87
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-05-25 11:32:43 +0300

    feat: solve the task (Common character count)

--- common_character_count.js
+++ common_character_count.js
@@ -6,12 +6,26 @@
 */
 
 //_Function:
-function getCommonCharacterCount(s1, s2) {}
+function getCommonCharacterCount(s1, s2) {
+  let counter = 0;
+  s1 = s1.split("");
+  s2 = s2.split("");
+
+  for (let i = 0; i < s1.length; i++) {
+    let index = s2.indexOf(s1[i]);
+    if (index !== -1) {
+      counter++;
+      delete s2[index];
+    }
+  }
+
+  return counter;
+}
 
 //_Tests:
 
 console.log(getCommonCharacterCount("aabcc", "adcaa"));
-//console.log(getCommonCharacterCount("aabdufaudfhauicc", "adcsdfsihdaa"));
-//console.log(getCommonCharacterCount("testString", "testString"));
-//console.log(getCommonCharacterCount("sbdfhbsdf", "gagd"));
-//console.log(getCommonCharacterCount("sbdfh", "gamasdsguigdigfgd"));
+console.log(getCommonCharacterCount("aabdufaudfhauicc", "adcsdfsihdaa"));
+console.log(getCommonCharacterCount("testString", "testString"));
+console.log(getCommonCharacterCount("sbdfhbsdf", "gagd"));
+console.log(getCommonCharacterCount("sbdfh", "gamasdsguigdigfgd"));

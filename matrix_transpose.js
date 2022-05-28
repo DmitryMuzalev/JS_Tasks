4a2849028e413414bdb4e7ec6cc0cc470ca8b927
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-05-28 14:25:31 +0300

    feat: solve the task (Matrix Transpose)

--- matrix_transpose.js
+++ matrix_transpose.js
@@ -13,7 +13,15 @@
 */
 
 //_Function:
-function transpose(matrix) {}
+function transpose(matrix) {
+  let result = matrix[0].map((e) => []);
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      result[j].push(matrix[i][j]);
+    }
+  }
+  return result;
+}
 
 //_Tests:
 console.log(transpose([[1]])); // [[1]]

d9ebdc6f575b4897d3381c5516cc644e01f010b5
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-05-28 16:31:11 +0300

    feat: solve the task (Island Count)

--- island_count.js
+++ island_count.js
@@ -20,7 +20,32 @@
 */
 
 //_Function:
-function countIslands(mapStr) {}
+function countIslands(mapStr) {
+  mapStr = mapStr.split("\n").map((e) => e.split(""));
+  let counter = 0;
+  for (let i = 0; i < mapStr.length; i++) {
+    for (let j = 0; j < mapStr.length; j++) {
+      if (mapStr[i][j] === "0") {
+        counter++;
+      }
+      fill(mapStr, i, j);
+    }
+  }
+  return counter;
+}
+
+function fill(map, i, j) {
+  if (i < 0 || i >= map.length) return;
+  if (j < 0 || j >= map[i].length) return;
+  if (map[i][j] === ".") return;
+  map[i][j] = ".";
+  fill(map, i - 1, j);
+  fill(map, i + 1, j);
+  fill(map, i, j - 1);
+  fill(map, i, j + 1);
+}
+
 //_Tests:
 console.log(countIslands("00...0\n0...00\n......\n0.0.0.\n0.....")); // 5
 console.log(countIslands("..000.\n..000.\n..000.\n.0....\n..000.")); // 3
+console.log(countIslands("0...0\n0...0\n00000")); // 1

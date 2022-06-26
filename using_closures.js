1f871b254da0a897c6f08c4c24eef2cda5b0240f
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-06-26 22:42:27 +0300

    init: Start working on a task Using closures to share class state

---
+++ using_closures.js
@@ -0,0 +1,22 @@
+//_Description:
+/* 
+   Summary of requirements:
+   
+   1. Cat constructor, requiring arguments for name and weight
+   2. Throw an error if name or weight not specified when invoking the constructor.
+   3. Cat.averageWeight() method should give the average weight of all cat instances created with Cat, even after if the instance's properties have changed.
+   4.Must use Object.defineProperty
+*/
+
+//_Solution:
+var Cat = (function () {})();
+
+//_Tests:
+let fluffy = new Cat("fluffy", 15);
+let garfield = new Cat("garfield", 25);
+
+console.log(fluffy.weight); //15
+console.log(fluffy instanceof Cat); //true
+console.log(fluffy.averageWeight); //undefined
+console.log(typeof Cat.averageWeight); //"function"
+console.log(Cat.averageWeight()); //20
